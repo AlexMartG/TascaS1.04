6c6b9b0788b075901817645bc3b4b9dfaee4c441
AlexMartG <amartinezg2@hotmail.com>
2026-05-19 18:46:58 +0200

    Level1 exercise1 completed

--- level1/exercise1.php
+++ level1/exercise1.php
@@ -3,8 +3,22 @@ class employee {
     public $name;
     public $salary;
 
+    public function setEmployee($name, $salary) {
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+    public function checkSalary() {
+        if ($this->salary > 6000) {
+            echo "Employee " . $this->name . " has to pay taxes.\n";
+        } else {
+            echo "Employee " . $this->name . " doesn't have to pay taxes.\n";
+        }
+    }
 }
-
-
+$employee = new employee();
+$employee->setEmployee("Antonio Guterres", 50000);
+echo "Employee Name: " . $employee->name . "\n";
+echo "<br>";
+$employee->checkSalary();
 
 ?>
